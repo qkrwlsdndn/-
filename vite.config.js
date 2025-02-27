ec6b55a65f519821556cdb7897e93fb649f141e8
qkrwlsdndn <fhfhfk12@naver.com>
2025-02-27 23:58:47 +0900

    asdf

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/pokemonn/",  
   build: {
-    outDir: "docs",
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.indexOf("node_modules") !== -1) {
-            const module = id.split("node_modules/").pop().split("/")[0];
-            return `vendor-${module}`;
-          }
-        },
-      },
-    },
-  },
-})
+    outDir: "dist"  
+  }
+});
